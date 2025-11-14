1273d10434ae589a81bc9b1d04db88626eb2a208
bolea <bastian.olea@subdere.gov.cl>
2025-11-14 11:11:25 -0300

    post mapas

--- _instrucciones.R
+++ _instrucciones.R
@@ -16,10 +16,10 @@ blogdown::new_post(title = "¿Por qué programar para trabajar con datos?",
 )
 
 # crear un post tutorial
-blogdown::new_post(title = "Desarrollando un buscador para mi blog con Shiny", 
-                   file = "blog/buscador/index.md",
+blogdown::new_post(title = "Portafolio: mapas de Áreas Metropolitanas", 
+                   file = "blog/mapas_areas_metropolitanas/index.md",
                    author = "Bastián Olea Herrera",
-                   tags = c("blog", "Shiny"),
+                   tags = c("blog", "mapas", "Chile", "gráficos"),
                    categories = c() 
 )
 
